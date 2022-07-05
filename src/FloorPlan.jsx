81c94119bf00ff0902208f4d622003c3e4315248
ilysim <illymarie1@live.com>
2022-07-05 15:25:57 -0500

    add components

---
+++ src/FloorPlan.jsx
@@ -0,0 +1,15 @@
+
+
+function FloorPlan() {
+  return (
+    <>
+      <h1>This is a floorplan.</h1>
+      <h2>Kitchen</h2>
+      <h2>LivingRoom</h2>
+      <h2>Bedroom</h2>
+      <h2>Bath</h2>
+    </>
+  )
+}
+
+export default FloorPlan
